0663e8cf9ca393adf1b8163b7e609c3b3eb79195
Iron-E <code.iron.e@gmail.com>
2022-07-31 22:26:54 -0400

    docs(Error): document variants

--- src/error.rs
+++ src/error.rs
@@ -5,34 +5,54 @@ use std::io;
 
 use thiserror::Error;
 
-/// An [`Error`](core::error::Error) for the crate.
+/// An [`Error`](std::error::Error) for the crate.
 #[derive(Debug, Error)]
 pub enum Error
 {
-	#[allow(missing_docs)]
+	/// The error was caused while performing operations on a [`Decimal`](crate::Decimal).
 	#[error(transparent)]
 	Decimal(#[from] rust_decimal::Error),
 
-	#[allow(missing_docs)]
+	/// The error was caused while trying to decode a value in a given `context` for a specific
+	/// `reason`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use money2::{Error::Decode, Money};
+	/// assert!(matches!(Money::try_from("20.00"), Err(Decode { .. })));
+	/// ```
 	#[error("There was an error decoding {context}: {reason}")]
 	Decode
 	{
-		context: String, reason: String
+		/// What was being decoded when this error occurred.
+		context: String,
+
+		/// The reason for this error.
+		reason: String,
 	},
 
-	#[allow(missing_docs)]
+	/// The error was caused while interacting with [`io`].
 	#[error(transparent)]
 	Io(#[from] io::Error),
 
-	#[allow(missing_docs)]
+	/// The error was caused while [`reqwest`]ing exchange rates from upstream.
 	#[error(transparent)]
 	Reqwest(#[from] reqwest::Error),
 
-	#[allow(missing_docs)]
+	/// A value had no equivalent [`Currency`] variant.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use money2::{Currency, Error::UnsupportedCurrency};
+	/// assert!(matches!(Currency::try_from("TMT"), Err(UnsupportedCurrency(_))));
+	/// ```
 	#[error("The {0} currency is not supported. See https://docs.rs/money2/latest/money2/type.Currency.html for a list of supported currencies")]
 	UnsupportedCurrency(String),
 
-	#[allow(missing_docs)]
+	/// The error was caused while dealing with a downloaded [`zip`] file containing raw exchange
+	/// rates.
 	#[error(transparent)]
 	Zip(#[from] zip::result::ZipError),
 }
